d26c8efa1c945236c0271a2adc8c81a4aa5d3d24
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2024-11-19 21:03:00 +0700

    feat(lexer): implements lexer

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"os"
+
+	"github.com/AungKyawPhyo1142/go-compiler/src/lexer"
 )
 
 func main() {
 	// read a file
 	bytes, _ := os.ReadFile("./example/00.lang")
-	source := string(bytes)
 
-	fmt.Printf("code: %s\n", source)
+	tokens := lexer.Tokenize(string(bytes))
+
+	for _, token := range tokens {
+		token.Debug()
+	}
+
 }
